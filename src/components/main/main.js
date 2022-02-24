3db232bf39eec953670f1a8c21def4619aadb98d
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2022-02-24 13:28:02 +0000

    serialising data

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -6,11 +6,12 @@ const Main = (props) => {
 
   const title = useRef()
   const text  = useRef()
+  const body  = useRef()
 
-  const t     = props.title
-  const b     = props.body
+  const titleText     = props.title
+  const bodyText     = props.body
 
-  const [truncated,setTruncation] = useState(false)
+  const [timeout, settimeout]     = useState(0)
   
   const onkeypress = (evt) => {
     const t = text.current.innerHTML
@@ -28,7 +29,44 @@ const Main = (props) => {
     return () => {
       title.current.removeEventListener('keypress',onkeypress)
     }
-  },[truncated])
+  },[])
+
+  const serialiseBody = () => {
+    // serialise body text to save
+    const children = [...body.current.children]
+    var text     = ""
+    children.map((e)=>{
+     var line = e.innerHTML
+     var broke = false
+     if (line === "<br>") {
+       line = "\\n"
+       broke = true
+     }
+     text += broke ? line : line + "\\n" 
+    })
+    return text
+  }
+
+  useEffect(()=> {
+    // save after 2 seconds of typing with timeout
+    var saved = false
+    const interval = setInterval(()=> {
+      if (new Date().getTime() > timeout) {
+        if (!saved) {
+          saved = true
+          const text = serialiseBody()
+          console.log(text)
+        }
+      }
+    },100)
+    return () => {
+      clearInterval(interval)
+    }
+  },[timeout])
+
+  const onTextChange = () => {
+    settimeout(new Date().getTime() + 2000)
+  }
 
   return(
     <Col style={{"overflow":"auto"}}>
@@ -37,13 +75,13 @@ const Main = (props) => {
           <Container fluid className='overflow-auto pl-5 main-inner' style={{"height":"100%"}}>
             <div ref={title} contentEditable="true" className='pt-2 main-title'>
               <p ref={text} className='py-1' style={{"text-overflow":"ellipsis","white-space":"nowrap","overflow":"hidden"}}>
-                {t}
+                {titleText}
               </p>
             </div>
             <hr></hr>
-            <div contentEditable className='overflow-hidden pb-3' onKeyPress={(e)=>console.log(e.target)}>
+            <div contentEditable className='overflow-hidden pb-3' ref={body} onInput={onTextChange} onKeyUp={onTextChange}>
               <p className='overflow-auto main-text p-0 m-0'>
-                {b}
+                {bodyText}
               </p>
             </div>
           </Container>
